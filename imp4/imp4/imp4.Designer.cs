d377a1a4505708cbb86408509264bea14b9a23dd
Billy Mosse <billy.mosse@gmail.com>
2014-08-05 18:49:35 -0300

    changed controls names so they have more information. Example: "button3" -> "btnCanny"

--- imp4/imp4/imp4.Designer.cs
+++ imp4/imp4/imp4.Designer.cs
@@ -28,95 +28,95 @@
         /// </summary>
         private void InitializeComponent ( )
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox ();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox ();
-            this.button1 = new System.Windows.Forms.Button ();
-            this.button3 = new System.Windows.Forms.Button ();
+            this.pctInput = new System.Windows.Forms.PictureBox ();
+            this.pctEdges = new System.Windows.Forms.PictureBox ();
+            this.btnUploadImage = new System.Windows.Forms.Button ();
+            this.btnCanny = new System.Windows.Forms.Button ();
             this.dOpen = new System.Windows.Forms.OpenFileDialog ();
-            this.textBox1 = new System.Windows.Forms.TextBox ();
-            this.label1 = new System.Windows.Forms.Label ();
-            ( (System.ComponentModel.ISupportInitialize)( this.pictureBox1 ) ).BeginInit ();
-            ( (System.ComponentModel.ISupportInitialize)( this.pictureBox2 ) ).BeginInit ();
+            this.txtThreshold = new System.Windows.Forms.TextBox ();
+            this.lblThreshold = new System.Windows.Forms.Label ();
+            ( (System.ComponentModel.ISupportInitialize)( this.pctInput ) ).BeginInit ();
+            ( (System.ComponentModel.ISupportInitialize)( this.pctEdges ) ).BeginInit ();
             this.SuspendLayout ();
             // 
-            // pictureBox1
+            // pctInput
             // 
-            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox1.Location = new System.Drawing.Point (12, 49);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size (330, 300);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.pctInput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pctInput.Location = new System.Drawing.Point (12, 49);
+            this.pctInput.Name = "pctInput";
+            this.pctInput.Size = new System.Drawing.Size (330, 300);
+            this.pctInput.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pctInput.TabIndex = 0;
+            this.pctInput.TabStop = false;
             // 
-            // pictureBox2
+            // pctEdges
             // 
-            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox2.Location = new System.Drawing.Point (542, 49);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size (330, 300);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
+            this.pctEdges.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pctEdges.Location = new System.Drawing.Point (542, 49);
+            this.pctEdges.Name = "pctEdges";
+            this.pctEdges.Size = new System.Drawing.Size (330, 300);
+            this.pctEdges.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pctEdges.TabIndex = 1;
+            this.pctEdges.TabStop = false;
             // 
-            // button1
+            // btnUploadImage
             // 
-            this.button1.Location = new System.Drawing.Point (12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size (75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Open";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler (this.button1_Click);
+            this.btnUploadImage.Location = new System.Drawing.Point (12, 12);
+            this.btnUploadImage.Name = "btnUploadImage";
+            this.btnUploadImage.Size = new System.Drawing.Size (75, 23);
+            this.btnUploadImage.TabIndex = 2;
+            this.btnUploadImage.Text = "Open";
+            this.btnUploadImage.UseVisualStyleBackColor = true;
+            this.btnUploadImage.Click += new System.EventHandler (this.btnUploadImage_Click);
             
             // 
-            // button3
+            // btnCanny
             // 
-            this.button3.Location = new System.Drawing.Point (406, 265);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size (75, 23);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Canny";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler (this.button3_Click);
+            this.btnCanny.Location = new System.Drawing.Point (406, 265);
+            this.btnCanny.Name = "btnCanny";
+            this.btnCanny.Size = new System.Drawing.Size (75, 23);
+            this.btnCanny.TabIndex = 4;
+            this.btnCanny.Text = "Canny";
+            this.btnCanny.UseVisualStyleBackColor = true;
+            this.btnCanny.Click += new System.EventHandler (this.btnCanny_Click);
             // 
             // dOpen
             // 
             this.dOpen.FileName = "openFileDialog1";
             // 
-            // textBox1
+            // txtThreshold
             // 
-            this.textBox1.Location = new System.Drawing.Point (433, 239);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size (75, 20);
-            this.textBox1.TabIndex = 5;
-            this.textBox1.Text = "80";
+            this.txtThreshold.Location = new System.Drawing.Point (433, 239);
+            this.txtThreshold.Name = "txtThreshold";
+            this.txtThreshold.Size = new System.Drawing.Size (75, 20);
+            this.txtThreshold.TabIndex = 5;
+            this.txtThreshold.Text = "80";
             // 
-            // label1
+            // lblThreshold
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point (373, 242);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size (54, 13);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Threshold";
+            this.lblThreshold.AutoSize = true;
+            this.lblThreshold.Location = new System.Drawing.Point (373, 242);
+            this.lblThreshold.Name = "lblThreshold";
+            this.lblThreshold.Size = new System.Drawing.Size (54, 13);
+            this.lblThreshold.TabIndex = 6;
+            this.lblThreshold.Text = "Threshold";
             // 
             // imp4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF (6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size (884, 362);
-            this.Controls.Add (this.label1);
-            this.Controls.Add (this.textBox1);
-            this.Controls.Add (this.button3);
-            this.Controls.Add (this.button1);
-            this.Controls.Add (this.pictureBox2);
-            this.Controls.Add (this.pictureBox1);
+            this.Controls.Add (this.lblThreshold);
+            this.Controls.Add (this.txtThreshold);
+            this.Controls.Add (this.btnCanny);
+            this.Controls.Add (this.btnUploadImage);
+            this.Controls.Add (this.pctEdges);
+            this.Controls.Add (this.pctInput);
             this.Name = "imp4";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Imp4";
-            ( (System.ComponentModel.ISupportInitialize)( this.pictureBox1 ) ).EndInit ();
-            ( (System.ComponentModel.ISupportInitialize)( this.pictureBox2 ) ).EndInit ();
+            ( (System.ComponentModel.ISupportInitialize)( this.pctInput ) ).EndInit ();
+            ( (System.ComponentModel.ISupportInitialize)( this.pctEdges ) ).EndInit ();
             this.ResumeLayout (false);
             this.PerformLayout ();
 
@@ -124,13 +124,13 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pctInput;
+        private System.Windows.Forms.PictureBox pctEdges;
+        private System.Windows.Forms.Button btnUploadImage;
+        private System.Windows.Forms.Button btnCanny;
         private System.Windows.Forms.OpenFileDialog dOpen;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtThreshold;
+        private System.Windows.Forms.Label lblThreshold;
     }
 }
 
