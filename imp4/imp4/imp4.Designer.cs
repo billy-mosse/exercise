553861714a43aab7aea053632bff3dbc3d46220b
Billy Mosse <billy.mosse@gmail.com>
2014-08-05 18:04:17 -0300

    Deleted sobel

--- imp4/imp4/imp4.Designer.cs
+++ imp4/imp4/imp4.Designer.cs
@@ -31,7 +31,6 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox ();
             this.pictureBox2 = new System.Windows.Forms.PictureBox ();
             this.button1 = new System.Windows.Forms.Button ();
-            this.button2 = new System.Windows.Forms.Button ();
             this.button3 = new System.Windows.Forms.Button ();
             this.dOpen = new System.Windows.Forms.OpenFileDialog ();
             this.textBox1 = new System.Windows.Forms.TextBox ();
@@ -69,16 +68,7 @@
             this.button1.Text = "Open";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler (this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point (406, 110);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size (75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Sobel";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler (this.button2_Click);
+            
             // 
             // button3
             // 
@@ -119,7 +109,6 @@
             this.Controls.Add (this.label1);
             this.Controls.Add (this.textBox1);
             this.Controls.Add (this.button3);
-            this.Controls.Add (this.button2);
             this.Controls.Add (this.button1);
             this.Controls.Add (this.pictureBox2);
             this.Controls.Add (this.pictureBox1);
@@ -138,7 +127,6 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.OpenFileDialog dOpen;
         private System.Windows.Forms.TextBox textBox1;
